d1a41fc6cfec8711300422a6d69a130abf002475
Тимур Пашакулов <@local.itstep>
2018-11-20 21:30:15 +0600

    Добавьте файлы проекта.

---
+++ Project1/Source.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <stdio.h>
+#include  <locale.h>
+
+
+
+int main()
+{
+	/*int a, b;
+	a = 34;
+	b = 56;
+	if (a < b) printf("%d\n", a);
+	else printf("%d/n", b)
+	
+	
+	
+	
+	
+	
+	
+	
+	/*int a, b;
+	a = 33;
+	b = 55;
+	if (a > b) printf("%d\n",a);
+	else printf("%d/n", b);*/
+
+
+
+
+
+
+
+
+
+
+
+
+	/*int r, d, e, a;
+	a = 5;
+	r = 6;
+	d = sqrt(a*a + a*a);
+	e = d / 2;
+	if (r < e) printf("yes\n");
+	else printf("no\n");*/
+
+
+
+
+	
+	
+	
+	/*int a, b, y, d, c, v;
+	a = 2;
+	b = 3;
+	y = a * b;
+	d = a / b;
+	c = a + b;
+    v = a - b;
+	printf("result: %d\n", y);
+	printf("result: %d\n", d);
+	printf("result: %d\n", c);
+	printf("result: %d\n", v);*/
+	
+	
+	/*int p, y, c;
+	p = 800;
+	y = 1000;
+	c = y / p;
+	printf("result: %d\n", c);*/
+
+
+
+
+	/*int a, b, c, s, d,k;
+	a = 6;
+	b = 5;
+	d = a + b;
+	c=d/2;
+	k = sqrt(a*b);
+	s = 2 * k;
+    printf("result: %d\n", c);
+	printf("result: %d\n", s);*/
+
+
+	
+	/*int a, b, p, f,d;
+	a = 2;
+	b = 6;
+	p = 2 * a + 2 * b;
+	f = a + b ;
+	d = f / 2;
+	printf("result: %d\n", p);
+	printf("result: %d\n", d);*/
+
+	/*int a, b, c,p;
+	a = 3;
+	b = 4;
+	c = sqrt(a*a + b * b);
+	p = a + b + c;
+	printf("result: %d\n", p);*/
+
+	/*int a, b, c;
+	a = 3;
+	b = 4;printf("result: %d\n", c);
+	c = sqrt(a*a + b * b);
+	printf("result: %d\n", c);*/
+
+	/*int d, p, s;
+	p = 3, 14;
+	d = 6;
+	s = p / 4 * d*d;
+	printf("result: %d\n", s);*/
+
+
+
+/*int l, r,p;
+p = 3, 14;
+r = 6;
+l = 2 * p*r;
+printf("result: %d\n", l)* /;
+system("pause");
+
+
+
+	/*int a, P;
+	a = 12;
+	P = 4* a;
+	printf("result: %d\n", P);*/;
+
+system("pause");
+}
